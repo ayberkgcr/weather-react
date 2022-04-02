2f1316e4dae303cb2dc75c199adcfde23aeb7970
ayberkgcr <deathgateway.ag@gmail.com>
2022-04-02 04:00:56 +0300

    Gibi

--- src/App.js
+++ src/App.js
@@ -64,9 +64,10 @@ function App() {
         <div className="weather-box">
           <div className="temp">
             {Math.round(weather.main.temp)}°c
+            Gibi
           </div>
           <div className="weather">{weather.weather[0].main}</div>
-
+          
         </div>
         </div>
 
